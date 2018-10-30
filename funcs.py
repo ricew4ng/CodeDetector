6289ac4b0dfc768b1c787a8a5440734bf4eb89a5
77Sera <sakainozomi77@gmail.com>
2018-10-30 22:15:12 +0800

    update

--- funcs.py
+++ funcs.py
@@ -3,103 +3,56 @@
 import numpy
 import re
 
-# 输入一个dict，通过库中每行token
-# dict[token] = list中的位置
-def trainDict(tokens,token_dict={},vector_length = 0):
-	for token in tokens:
-		if token not in token_dict.keys():
-			token_dict[token] = vector_length
-			vector_length+=1
-	return token_dict,vector_length
-
-# 根据训练的token字典和相应的向量长度，将tokens转成一个vector
-def tokens2vector(tokens,token_dict,vector_length):
-	vector = numpy.zeros(vector_length)
-	vector = vector.astype(numpy.int16)
-	
-	for token in tokens:
-		if token in token_dict.keys():
-			vector[ token_dict[token] ] = 1
-	return vector
-	
-# 根据向量转成tokens
-def vector2tokens(vector,token_vkdict):
-	tokens = []
-	for i in range(len(vector)):
-		if vector[i] == 1:
-			tokens.append(token_vkdict[i])
-	return tokens
-	
-# 以各个间隔符分割一行代码，返回一个包含间隔符的token的list
 # 字符串和普通变量名应该各自用一个token表示，比如STRING，VAR
-def code2tokens(code):
-	split_list = ['.','\'','"',' ','[',']','(',')','{','}',':','=',',']
-	pat_string = '\'(.*?)\'|"(.*?)"|\'\'\'(.*?)\'\'\'' 
-	pat_var = '.*?='
+def replaceCode(code):
+	# 先把变量名，字符串，数字替换成 ROVAR,ROSTRING,RONUM
+	pat_string = '\'(.+?)\'|"(.+?)"|\'\'\'(.+?)\'\'\'' 
 	pat_num = '[0-9]+?'
+	pat_var1 = '[a-zA-Z_]+? *='
+	pat_var2 = '[a-zA-Z_]+? *,'
+	pat_var3 = '[a-zA-Z_]+? *\)'
+
+	code = re.compile(pat_string).sub('"ROSTRING"',code)
+	code = re.compile(pat_num).sub('RONUM',code)
+	code = re.compile(pat_var1).sub('ROVAR=',code)
+	code = re.compile(pat_var2).sub('ROVAR,',code)
+	code = re.compile(pat_var3).sub('ROVAR)',code)
 	
+	return code
+	
+# 分割库中代码用，以各个间隔符分割一行代码，返回一个包含间隔符的token的list
+def code2tokens(code):
 	tokens = []
 	
-	# 将字符串替换为 "STRING"，将变量替换为 SERA_VAR=
-	code = re.compile(pat_string).sub('"SERA_STRING"',code) 
-	code = re.compile(pat_var).sub('SERA_VAR=',code)
-	code = re.compile(pat_num).sub('SERA_NUM',code)
-	
+	split_list = ['.','\'','"',' ','[',']','(',')','{','}',':','=',',','+','-','>','<']
 	word = ''
 	for i in code:
-		if i in split_list:
+		if i not in split_list:
+			word+=i
+		else:
 			if word != '':
 				tokens.append(word)
 				word = ''
 			if i != ' ':
 				tokens.append(i)
-		else:
-			word+=i
+			
 	return tokens
 			
-# 输入两个向量(list)，应用余弦定理求出其余弦值
-# 公式: cos值 = <vectorA,vectorB> / ( |vectorA|*|vectorB| )
-# 即: cos值 = ( x1y1+x2y2+...+xNyN ) / sqrt(x1*x1+x2*x2+...+xN*xN) * sqrt(y1*y1+y2*y2+...+yN*yN)
-def getCosine(vectorA,vectorB):
-	value1 = 0
-	sumA = 0
-	sumB = 0
-	
-	for i in range(len(vectorA)):
-		value1+=(vectorA[i]*vectorB[i])
-		sumA+=pow(vectorA[i],2)
-		sumB+=pow(vectorB[i],2)
-	
-	value2 = pow(sumA,0.5) * pow(sumB,0.5)
-	
-	if value2 == 0:
-		r = 0
-	else:
-		r = value1 / value2
-	return r
-	
 # 从目标测试文件中读取每行代码，返回一个list（string）
-def getCodes(file_path):
+def getCodes(file_path,isJson=True):
 	try:
-		pat = '"snippet": "(.*?)"'
-		with open(file_path,'r') as file:
-			text = file.read()
-		r = re.compile(pat).findall(text)
+		if isJson:
+			pat = '"snippet": "(.*?)",'
+			with open(file_path,'r') as file:
+				text = file.read()
+			codes = re.compile(pat).findall(text)
+		else:
+			# codes = []
+			# with open(file_path,'r') as file:
+				# for line in file:
+					
+					# codes.append()
+			pass
 	except Exception as e:
 		raise e
-	return r
-	
-# 从字典文件中读取字典，字典格式是 key \t value \n
-# 返回两个dict，第一个是 key-value，第二个是value-key 因为两个字典都是唯一的
-def getDict(dict_path):
-	try:
-		dict = {}
-		dict2 = {}
-		with open(dict_path,'r') as file:
-			for line in file:
-				kv = line.replace('\n','').split('\t')
-				dict[kv[0]] = int(kv[1])
-				dict2[ int(kv[1]) ] = kv[0] 
-	except Exception as e:
-		raise e
-	return dict,dict2
+	return codes
--- main.py
+++ main.py
@@ -1,84 +1,82 @@
 #coding:utf8
 
 from funcs import *
+''' 
+新的方法：
+1. 先将conala-test.json的代码用 replaceCode方法保证语义不变的情况下，替换成相对"泛化"一点的代码，暂时保存在文件里
+		
+2. 然后对于一段输入的代码，做以下操作:
 
-# 最终目的是将库中每行代码对应一个向量，即生成一个向量矩阵存入文件中，还有一个dict
-# 同时，测试代码要比较相似度的话，初始化一个全零list，需要通过一个dict，来给list相应位置1
-
-# 如果要训练的话，就是看dict中是否有未知token，然后添加入dict中，并给list置1
-
-# 总结一下 语法树，一般来说
-# 等号左边都是给某个变量赋值，所以可以把等号左边的变量都替换为某个定值
+	a) 先把字符串，普通变量名之类的常量做替换，
+	b) 用code2tokens函数切割代码为一个个token组成的list
+	c) 遍历这个token_list，每次用正则在文件中匹配有这个token的行代码，返回行序号
+	d) 对于每个token，都有一个由行序号组成的list
+	e) 对这些list，找出它们之间的重合度最高的（就是这个token_list里的token在哪一行代码里出现的最多），生成一个dict，键是行序号，值是次数
+	f) 也就是 次数越大，就跟目标代码越相似
+	
+	注: 考虑可以优化的地方，就是token的优先级。
+'''
 
 if __name__ == '__main__':
-
-	token_dict = getDict('data/output_dict') # 载入字典
-
-	vector_length = 75162
-
-	codes_train = getCodes('data/conala-train.json') # 载入raw代码
-
-	print('[*] Training vectors...')
-
-	vectorsA = []  # 初始化库中的vector数组
-
-	count = 0
-	for code in codes_train:
-		tokens = code2tokens(code)  # 将代码转成token数组
-		vector = tokens2vector(tokens, token_dict, vector_length)
-
-		vectorsA.append(vector)
-		count += 1
-		if count == 50:
-			break
-
-	print('[*] VectorsA ready !')
-
-	print('[*] Processing test codes...')
-
-	codes = getCodes('data/conala-test.json')  # 载入测试代码
-
-	count = 0  # 测试3行代码
-	train_index = 0
-
-	for code in codes:
-		tokens = code2tokens(code)
-		vectorB = tokens2vector(tokens, token_dict, vector_length)  # 获取待测试代码的向量
-
-		max_sim = 0
-
-		# 遍历 vectorsA ，与vectorB做比较
-		for idx, vectorA in enumerate(vectorsA):
-			r = getCosine(vectorA, vectorB)
-			if max_sim < r:
-				max_sim = r
-				train_index = idx
-
-		print("-------------")
-		print('[*] Result ' + str(count + 1) + '=>' + str(max_sim) + '\ncode=>' + ''.join(tokens)
-			  + '\ncode_base=>' + ''.join(codes_train[train_index]))
-		print('base_code_index => ' + str(train_index))
-
-		count += 1
-		if count == 20:
-			break
-
-
-	# x = map(lambda i:str(i) ,x )
-
-	# with open('test','w') as file:
-	# file.write( ','.join(x)+'\n' )
-
-	'''# 训练token字典，比较耗时，几十秒左右
-	codes = getCodes('./data/conala-train.json')
-
-	vector_length = 0
-
-	for code in codes:
-		tokens = code2tokens(code) # 获得此tokens
-		token_dict,vector_length = trainDict(tokens,token_dict,vector_length)
-
-	with open('output_dict','w') as file:
-		for k,v in token_dict.items():
-			file.write(k+'\t'+str(v)+'\n')
+	with open('tokens_test','r') as file:
+		text = file.read()
+	
+	# 测试代码：
+	code = 'x = int(my_string.replace("ROSTRING", ""))'
+	code = replaceCode(code)
+	tokens = code2tokens(code)
+	
+	dict = {}
+	for token in tokens:
+		try:
+			pat = '([0-9]+?) .*?'+token+'.*?\n'
+			r = re.compile(pat).findall(text)
+		except:
+			pat = '([0-9]+?) .*?\\'+token+'.*?\n'
+			r = re.compile(pat).findall(text)
+			
+		for i in r:
+			if i in dict.keys():
+				dict[i]+=1
+			else:
+				dict[i] = 1
+	
+	max_kvs = [] # 二维list
+	for i in range(5):
+		kv = [-1,0]
+		max_kvs.append(kv)
+	
+	for key,value in dict.items():
+		for i in range(len(max_kvs)):
+			if value > max_kvs[i][1]:
+				max_kvs.insert(i,[key,value])
+				max_kvs.pop(5)
+				break
+	
+	for i in max_kvs:
+		print( i[0],i[1] )
+	
+	
+	
+	# codes_train = getCodes('data/conala-train.json')
+	'''
+	codes_test = getCodes('data/conala-test.json')
+	
+	with open('tokens_test','w') as file:
+		for i,code in enumerate(codes_test):
+			code = replaceCode(code)
+			file.write(str(i)+' '+code+'\n' )
+	'''
+	
+	
+	
+	'''
+	with open('data/codes_test','w') as file:
+		for i,code in enumerate(codes_test):
+			file.write(str(i)+' '+code+'\n' )
+			
+	with open('tokens_test','w') as file:
+		for i,code in enumerate(codes_test):
+			tokens = code2tokens(code)
+			file.write(str(i)+' '+' '.join(tokens)+'\n' )
 	'''
