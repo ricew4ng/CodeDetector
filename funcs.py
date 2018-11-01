193faa1d19597386a4be2ab291147d3f982b480a
77Sera <sakainozomi77@gmail.com>
2018-11-01 20:44:04 +0800

    Update

--- funcs.py
+++ funcs.py
@@ -1,24 +1,62 @@
 #coding:utf8
 
-import numpy
 import re
 
 # 字符串和普通变量名应该各自用一个token表示，比如STRING，VAR
 def replaceCode(code):
 	# 先把变量名，字符串，数字替换成 ROVAR,ROSTRING,RONUM
 	pat_string = '\'(.+?)\'|"(.+?)"|\'\'\'(.+?)\'\'\'' 
-	pat_num = '[0-9]+?'
+	pat_num = '[0-9]+'
 	pat_var1 = '[a-zA-Z_]+? *='
 	pat_var2 = '[a-zA-Z_]+? *,'
 	pat_var3 = '[a-zA-Z_]+? *\)'
-
 	code = re.compile(pat_string).sub('"ROSTRING"',code)
 	code = re.compile(pat_num).sub('RONUM',code)
 	code = re.compile(pat_var1).sub('ROVAR=',code)
 	code = re.compile(pat_var2).sub('ROVAR,',code)
 	code = re.compile(pat_var3).sub('ROVAR)',code)
-	
 	return code
+			
+# 通过tokens，对dict进行更新,weight是（用行次数表示）权重值，用高优先级token匹配则权重应大一点
+def generateDict(tokens=[],text='',dict={},weight=1):
+	for token in tokens:
+		# r 是list，其中,r[x][0] 是序号+对应行代码；r[x][1]是行序号
+		try:
+			pat = '(([0-9]+?) .*?'+token+'.*?\n)'
+			r = re.compile(pat).findall(text)
+		except:
+			pat = '(([0-9]+?) .*?\\'+token+'.*?\n)'
+			r = re.compile(pat).findall(text)
+		for k in r:
+			if k[1] in dict.keys():
+				dict[k[1]][1]+=weight
+			else:
+				# 初始化dict的键为某行数的值，为一个list，list[0]是爬到的文本,list[1]是行出现次数
+				dict[k[1]] = [k[0],1] 
+	return dict
+	
+# 暂时通过文件读取（后面改成数据库
+def getTextByNum(tokens,num_list):
+	pass
+	
+# 输入tokens列表，返回按照优先级划分的2个list
+def splitTokensByPRI(tokens):
+	h_PRI = ['.','(','['] # 这些字符前的词权重应该是最高的
+	low_list = ['.','\'','"',' ','[',']','(',')','{','}',':','=',',','+','-','>','<','RONUM','ROSTRING','ROVAR','%']
+	alpha_list = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	
+	h_tokens = [] # 高优先级 tokens
+	l_tokens = [] # 低优先级 tokens
+	
+	for i,token in enumerate(tokens):
+		# 如果当前token是 .([中的一个，则前一个token优先级很高
+		if token in h_PRI and i != 0: 
+			if tokens[i-1] not in low_list and tokens[i-1] not in alpha_list:
+				h_tokens.append(tokens[i-1])
+		else:
+			l_tokens.append(token)
+	
+	return h_tokens,l_tokens
 	
 # 分割库中代码用，以各个间隔符分割一行代码，返回一个包含间隔符的token的list
 def code2tokens(code):
@@ -38,8 +76,19 @@ def code2tokens(code):
 			
 	return tokens
 			
+# 输入codes，（原始无序号，每行为一行代码）
+# 输出一份对应的带行号，且每行都是高优先级token 的文本
+def generateTokensByPRI(codes,fileName='data/high_tokens_id'):
+	with open(fileName,'w') as file:
+		for i,code in enumerate(codes):
+			code = replaceCode(code)
+			tokens = code2tokens(code)
+			h_tokens,l_tokens = splitTokensByPRI(tokens)
+			
+			file.write( str(i)+' '+' '.join(h_tokens)+'\n' )
+			
 # 从目标测试文件中读取每行代码，返回一个list（string）
-def getCodes(file_path,isJson=True):
+def getCodes(file_path,isJson=False):
 	try:
 		if isJson:
 			pat = '"snippet": "(.*?)",'
@@ -47,12 +96,10 @@ def getCodes(file_path,isJson=True):
 				text = file.read()
 			codes = re.compile(pat).findall(text)
 		else:
-			# codes = []
-			# with open(file_path,'r') as file:
-				# for line in file:
-					
-					# codes.append()
-			pass
+			codes = []
+			with open(file_path,'r') as file:
+				for line in file:
+					codes.append(line[:-1])
 	except Exception as e:
 		raise e
 	return codes
--- main.py
+++ main.py
@@ -1,82 +1,49 @@
 #coding:utf8
 
 from funcs import *
-''' 
-新的方法：
-1. 先将conala-test.json的代码用 replaceCode方法保证语义不变的情况下，替换成相对"泛化"一点的代码，暂时保存在文件里
-		
-2. 然后对于一段输入的代码，做以下操作:
-
-	a) 先把字符串，普通变量名之类的常量做替换，
-	b) 用code2tokens函数切割代码为一个个token组成的list
-	c) 遍历这个token_list，每次用正则在文件中匹配有这个token的行代码，返回行序号
-	d) 对于每个token，都有一个由行序号组成的list
-	e) 对这些list，找出它们之间的重合度最高的（就是这个token_list里的token在哪一行代码里出现的最多），生成一个dict，键是行序号，值是次数
-	f) 也就是 次数越大，就跟目标代码越相似
-	
-	注: 考虑可以优化的地方，就是token的优先级。
-'''
 
 if __name__ == '__main__':
-	with open('tokens_test','r') as file:
-		text = file.read()
+	# 生成高优先级tokens 文本 high_tokens_id
+	# codes = getCodes('data/codes_train')
+	# generateTokensByPRI(codes)
+		
+	# 载入高优先级库文本
+	with open('data/high_tokens_id','r') as file:
+		high_tokens_text = file.read()
+		
+	codes_train = []
+	with open('data/codes_train','r') as file:
+		for line in file:
+			codes_train.append(line[:-1])
+			
+	# 输入待检测代码
+	code = 'urllib.parse.unquote(\'Foo%E2%84%A2%20Bar\').decode(\'utf-8\')'
 	
-	# 测试代码：
-	code = 'x = int(my_string.replace("ROSTRING", ""))'
-	code = replaceCode(code)
 	tokens = code2tokens(code)
 	
-	dict = {}
-	for token in tokens:
-		try:
-			pat = '([0-9]+?) .*?'+token+'.*?\n'
-			r = re.compile(pat).findall(text)
-		except:
-			pat = '([0-9]+?) .*?\\'+token+'.*?\n'
-			r = re.compile(pat).findall(text)
-			
-		for i in r:
-			if i in dict.keys():
-				dict[i]+=1
-			else:
-				dict[i] = 1
+	h_tokens,l_tokens = splitTokensByPRI(tokens)
+	
+	dict = generateDict(h_tokens,text=high_tokens_text,weight=50)
+	
+	filt_text = ''
+	
+	for line_num in dict.keys():
+		filt_text+=codes_train[int(line_num)]+'\n'
+		
+	dict = generateDict(l_tokens,text=filt_text,weight=0.1)
 	
 	max_kvs = [] # 二维list
-	for i in range(5):
+	for i in range(10):
 		kv = [-1,0]
 		max_kvs.append(kv)
 	
 	for key,value in dict.items():
 		for i in range(len(max_kvs)):
-			if value > max_kvs[i][1]:
-				max_kvs.insert(i,[key,value])
-				max_kvs.pop(5)
+			if value[1] > max_kvs[i][1]:
+				max_kvs.insert(i,[key,value[1]])
+				max_kvs.pop(10)
 				break
 	
+	print('[*] \tbase code=>'+code)
 	for i in max_kvs:
-		print( i[0],i[1] )
-	
-	
-	
-	# codes_train = getCodes('data/conala-train.json')
-	'''
-	codes_test = getCodes('data/conala-test.json')
-	
-	with open('tokens_test','w') as file:
-		for i,code in enumerate(codes_test):
-			code = replaceCode(code)
-			file.write(str(i)+' '+code+'\n' )
-	'''
-	
-	
-	
-	'''
-	with open('data/codes_test','w') as file:
-		for i,code in enumerate(codes_test):
-			file.write(str(i)+' '+code+'\n' )
-			
-	with open('tokens_test','w') as file:
-		for i,code in enumerate(codes_test):
-			tokens = code2tokens(code)
-			file.write(str(i)+' '+' '.join(tokens)+'\n' )
-	'''
+		print( i[0]+' '+str(i[1])+'\tcode=>'+codes_train[int(i[0])] )
